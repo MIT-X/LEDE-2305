7b4f802a6fb25f89fa1d6435f13e31fa4f74eb72
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-24 12:54:22 +0800

    Add files via upload

---
+++ DIY-2.sh
@@ -0,0 +1,12 @@
+# 此脚本用处是：定制个性化参数
+#=====================================================================
+
+# 1-替换新版 golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+
+# 2-设置密码为空
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
+
+# 3-修改内核版本
+sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' ./target/linux/x86/Makefile
