6cafaa826aab266547eee48b71221963bd9f1ff0
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-25 15:59:01 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -1,6 +1,7 @@
 # 此脚本用处是：定制个性化参数
 #=====================================================================
-
+# 1-更改默认地址
+sed -i 's/192.168.1.1/192.168.2.222/g' package/base-files/files/bin/config_generate
 
 # 2-设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
