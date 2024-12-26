54937de6a0d9443b07043ea43e639c7011ccb90f
MIT-X <134132915+MIT-X@users.noreply.github.com>
2024-12-26 15:15:30 +0800

    Update DIY-2.sh

--- DIY-2.sh
+++ DIY-2.sh
@@ -7,7 +7,7 @@ rm -rf ./feeds/luci/applications/luci-app-passwall2 && cp -rf ./feeds/passwall2 
 rm -rf ./feeds/luci/applications/luci-app-openclash && cp -rf ./feeds/openclash ./feeds/luci/applications/luci-app-openclash
 
 # 1-更改默认地址
-# sed -i 's/192.168.1.1/192.168.2.222/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.222/g' package/base-files/files/bin/config_generate
 
 # 2-设置密码为空
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' package/lean/default-settings/files/zzz-default-settings
